84ddeb2cb93439b28a0b6ce849dce9230c977ae5
BloodJuice <falcon.with.hat@gmail.com>
2023-08-28 12:37:12 +0300

    Continue working

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-import math
 import matplotlib.pyplot as plt
 import numpy as np
-from scipy.optimize import minimize, Bounds, LinearConstraint, NonlinearConstraint
+
 
 def calculateYwithoutError(t, x1, x2, size):
     y_ = np.zeros((size, ))
@@ -20,16 +18,18 @@ def Painting(x, y):
 
 def ylinealModel(n, tetta, outlier):
     # Search y without observation error
-    x1 = np.random.uniform(0, 10.0, n)
-    x2 = np.random.uniform(0, 10.0, n)
+    x1 = np.random.uniform(0., 10.0, n)
+    x2 = np.random.uniform(0., 10.0, n)
+
     y = calculateYwithoutError(tetta, x1, x2, n)
+    xall = []
 
     # Search observation error
-    e = np.random.binomial(n=1, p=(1 - outlier), size=n)
+    e = np.random.binomial(n=1., p=(1 - outlier), size=n)
 
     # Search y_res:
     varMainObservations = 0.01
-    varEmissions = 0.5
+    varEmissions = 5.
     y_res = np.zeros((n,))
 
     for i in range(n):
@@ -37,55 +37,58 @@ def ylinealModel(n, tetta, outlier):
             y_res[i] = y[i] + np.random.normal(0, np.sqrt(varMainObservations))
         else:
             y_res[i] = y[i] + np.random.normal(0, np.sqrt(varEmissions))
-    return y_res
+    for i in range(n):
+        xall.append(x1[i])
+    for i in range(n):
+        xall.append(x2[i])
+    return y_res, xall
 
-def lineToColum(x, n):
+def lineToColum(x, n, tetta):
     # Преобразование входной строки x в матрицу
-    count = int(len(x) / n)
-    saverDraft, xnew = [], []
-    for stepi in range(count):
+    nTetta = len(tetta) - 1
+    xnew = np.zeros((nTetta, n))
+    counterDel = 0
+    for stepi in range(nTetta):
         for stepj in range(n):
-            saverDraft.append(x[stepj])
-        xnew.append(saverDraft)
-        saverDraft = []
-    xnew = np.array(xnew).reshape(count, n)
+            xnew[stepi][stepj] = x[counterDel]
+            counterDel += 1
     return xnew
 
-def LSM(x, params):
-    y = params['y'].copy()
-    t = params['tetta'].copy()
-    n = params['n']
-    e = 0
+def LMSMatrix(x, y):
+    return np.dot((np.dot(np.linalg.inv(np.dot(x.transpose(), x)), x.transpose())), y)
 
-    xNew = lineToColum(x, n)
-    print("\nHere...\n")
+def filingMatrixX(x, xall, tetta):
+    nTetta = len(tetta)
+    n = len(x[0])
+    for stepi in range(n):
+        x[0][stepi] = 1.
+    for stepi in range(1, nTetta):
+        for stepj in range(n):
+            x[stepi][stepj] = xall[stepi - 1][stepj]
+    # for stepi in range(nTetta):
+    #     for stepj in range(n):
+    #         for stepz in range(stepj, n):
+    #             x[stepi][stepj] += x[stepi][stepz]
+    return x.transpose()
 
-    # Расчёт значения ошибки
-    for i in range(n):
-        e += pow(y[i] - (t[0] + t[1] * xNew[0][i] + t[2] * xNew[1][i]), 2)
-    return e
-
-def Minimize(tetta, y, n, count): # count - число иксов
-    xAll = np.zeros((n * count))
-    print("\nminimise...\n")
-    res = minimize(fun=LSM, x0=xAll,args={"y": y, "tetta": tetta, "n": n}, method='TNC')
-    res = lineToColum(np.array(res.__getitem__('x')), n)
-    return res
+def relativeError(tettaTrue, tettanew):
+    n, counter = len(tettaTrue), 0
+    for stepi in range(n):
+        counter += pow((tettaTrue[stepi] - tettanew[stepi]), 2) / pow(tettaTrue[stepi], 2)
+    return counter
 
 def main():
-    N, tetta = 200, np.array([1., 1.5, 2.])
+    N, tetta, tettaNew = 200, np.array([1., 1.5, 2.]), np.array([0., 0., 0.])
+
     Outlier = 0.
-    yTrue = ylinealModel(n=N, tetta=tetta, outlier=Outlier)
-
-    print("\nres:\n", Minimize(tetta, yTrue, N, (len(tetta) - 1)))
-
-
-
-
-
-
-
-
+    while Outlier < 0.25:
+        yTrue, xAll = ylinealModel(n=N, tetta=tetta, outlier=Outlier)
+        X = np.zeros((len(tetta), N))
+        X = filingMatrixX(X, lineToColum(xAll, N, tetta), tetta)
+        tettaNew = LMSMatrix(X, yTrue.reshape(N, 1))
+        print("\nOutlier = ", Outlier*100,"%", "\ntetta:\n", tettaNew)
+        print("\nОтносительная ошибка:\n", relativeError(tettaTrue=tetta, tettanew=tettaNew))
+        Outlier += 0.05
 
 if __name__ == '__main__':
     main()
