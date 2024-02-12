2cbbb0d3a95e2393b9476a9f02ab9371690901af
Ice Cream <falcon.with.hat@gmail.com>
2024-02-12 22:17:14 +0700

    C0

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ import M_Estimators as MEst
 import ExtraThings as ex
 
 import matplotlib.pyplot as plt
+
+
 def filingMatrixX(xall, n):
     Xsaver = []
     for i in range(n):
@@ -28,9 +30,10 @@ def MiddleTettas(tettas):
         summa[i] /= n
     return summa
 
+
 def main():
     n, tetta, p = 500, np.array([1., 1.5, 2.]), 3
-    limit = 10.0
+    limit = 1.0
     Outlier = 0.05
     varMainObservations = 0.1
     varEmissions = 1.
@@ -44,11 +47,10 @@ def main():
     mcdMethod_test = MCD_Test.MCD()
     mcdMethod_three_var = MCD_Three_variables.MCD()
 
-    Ncycle = 2
+    Ncycle = 50
 
     while Outlier <= 0.25:
         LSsaver = []
-        MCDsaver = []
         MCDsaver_ = []
         Hubersaver = []
         Cauchysaver = []
@@ -56,25 +58,17 @@ def main():
         h = int((1. - Outlier) * n)
 
         for i in range(Ncycle):
-
             Y, xAll = LSObject.ylinealModel(n, tetta, Outlier, limit, varMainObservations, varEmissions)
-
 
             xVectorMCD_, yVectorMCD_ = mcdMethod_three_var.FindRelativeDistances(X=xAll, Y=Y, n=n, h=h)
             xMatrixMCD_ = filingMatrixX(xall=xVectorMCD_, n=h)
             tettaMCD_ = LSObject.LSMatrix(xMatrixMCD_, yVectorMCD_)
             MCDsaver_.append(tettaMCD_.copy())
 
-            # xVectorMCD, yVectorMCD = mcdMethod.FindRelativeDistances(X=xAll, Y=Y, n=n, h=h)
-            # xMatrixMCD = filingMatrixX(xall=xVectorMCD, n=h)
-            # tettaMCD = LSObject.LSMatrix(xMatrixMCD, yVectorMCD)
-            # MCDsaver.append(tettaMCD.copy())
-
             X = filingMatrixX(xAll, n)
             tettaLS = LSObject.LSMatrix(X, Y)
             LSsaver.append(tettaLS.copy())
             # tettaLSTest = np.linalg.lstsq(X, Y, rcond=None)[0]
-
 
             tettaMEstHuber = MObject.MainEstimators(tettaLS, "Huber", X, Y, n)
             Hubersaver.append(tettaMEstHuber.copy())
@@ -82,16 +76,16 @@ def main():
             tettaMEstCauchy = MObject.MainEstimators(tettaLS, "Cauchy", X, Y, n)
             Cauchysaver.append(tettaMEstCauchy.copy())
             print(f" i == {i}\n")
-        
+
         extraObj = ex.ExtraThings()
         iLS.append(extraObj.MainCount(LSsaver, tetta, Ncycle)[0])
-        iMCD.append(extraObj.MainCount(MCDsaver, tetta, Ncycle)[0])
+        iMCD.append(extraObj.MainCount(MCDsaver_, tetta, Ncycle)[0])
         iHuber.append(extraObj.MainCount(Hubersaver, tetta, Ncycle)[0])
         iCauchy.append(extraObj.MainCount(Cauchysaver, tetta, Ncycle)[0])
 
-        print(f" i == {n}\n")
+        print(f" i == {Outlier}\n")
         outSaver.append(Outlier)
-        Outlier += 0.05
+        Outlier += 0.03
 
         # nSaver.append(n)
         # n += 40
@@ -103,13 +97,15 @@ def main():
     plt.plot(outSaver, iLS,
              outSaver, iMCD,
              outSaver, iHuber,
-             outSaver, iCauchy)  # построение графика
+             outSaver, iCauchy
+             )  # построение графика
     plt.legend(("LS",
                 "MCD",
                 "Huber",
-                "Cauchy"))
+                "Cauchy"
+                ))
     plt.show()
+
 
 if __name__ == '__main__':
     main()
-
