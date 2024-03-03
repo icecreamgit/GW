ffe5ae5b5213140df645ae48f2088a5859bdd0c8
Ice Cream <falcon.with.hat@gmail.com>
2024-03-03 21:49:34 +0700

    C0

--- main.py
+++ main.py
@@ -3,16 +3,29 @@ import stands.StandForFourMethods as StandForFourMethods
 import stands.StandForDistansesMCD as StandForDistansesMCD
 
 def main():
-    n, tetta, p = 500, np.array([1., 1.5, 2.]), 3
+    n, tetta, p = 250, np.array([1., 1.5, 2.]), 3
     limit = 1.0
-    outlier = 0.05
-    nCycle = 10
+    outlier = 0.1
+    nCycle = 1
+
+    # grafic - отображение зависимости кси от выбросов
+    # oneOutput - вывод в консоль значений показателей точности оценивания
+    # для LS, MCD, M-estimators of Cauchy and Huber
+    modeForGrafic = "oneOutput"
 
     # Only: "normalModel" "cauchyModel" "exponentModel"
-    mode = "exponentModel"
-
+    modes = ["normalModel", "cauchyModel", "exponentModel"]
     # For I model (only normal)
-    emissionZones = [0.1, 0.5, 1., 1.5]
+    # emissionZones = [[0., 0.01, 0., 0.25], [0., 0.25, 0., 5.]]
+    # mode = "normalModel"
+
+    # For II model (normal + cauchy)
+    # emissionZones = [[0., 0.01, 0., 0.5], [0., 0.25, 0., 0.5], [0., 0.01, 0., 1.], [0., 0.25, 0., 1.]]
+    # mode = "cauchyModel"
+
+    # For III model (normal + exp)
+    emissionZones = [[0., 0.1, 0., 0.5], [0., 0.25, 0., 0.5], [0., 0.01, 0., 1.], [0., 0.25, 0., 1.], [0., 0.01, 0., 2.], [0., 0.25, 0., 2.]]
+    mode = "exponentModel"
 
 
     standForFourMethods = StandForFourMethods.StandForFourMethods()
@@ -20,9 +33,16 @@ def main():
 
     params = {"n": n, "tetta": tetta, "outlier": outlier, "limit": limit,
               "emissionZones": emissionZones, "nCycle": nCycle}
-
-    standForFourMethods.Main_StandForFourMethods(params, mode)
-    # standForDistansesMCD.Main_StandForDistansesMCD(params)
+    for mode in modes:
+        for emissionZone in emissionZones:
+            params["emissionZones"] = emissionZone
+            params["n"] = n
+            while n <= 500:
+                standForFourMethods.Main_StandForFourMethods(params, mode, modeForGrafic)
+                # standForDistansesMCD.Main_StandForDistansesMCD(params)
+                n += 50
+                params["n"] = n
+            n = 250
 
 
 
--- stands/StandForFourMethods.py
+++ stands/StandForFourMethods.py
@@ -55,9 +55,12 @@ class StandForFourMethods:
         ))
         plt.savefig(path + fileName)
         plt.show()
+    def __CreateFileForIndexes(self, name, path, LS, MCD, Huber, Cauchy, message):
+        with open(f"{path}{name}.txt", "w") as file:
+            file.write(message)
+            file.write(f"\nLS: {LS}\nMCD: {MCD}\nHuber: {Huber}\nCauchy: {Cauchy}")
 
-
-    def Main_StandForFourMethods(self, params, mode):
+    def Main_StandForFourMethods(self, params, mode, modeForGrafic):
         n = params["n"]
         tetta = params["tetta"]
         outlier = params["outlier"]
@@ -74,14 +77,57 @@ class StandForFourMethods:
         MObject = MEst.M_Estimators()
         mcdMethod_three_var = MCD_Three_variables.MCD()
 
-        while outlier <= 0.25:
-            LSsaver = []
-            MCDsaver_ = []
-            Hubersaver = []
-            Cauchysaver = []
+        LSsaver = []
+        MCDsaver_ = []
+        Hubersaver = []
+        Cauchysaver = []
 
-            params["outlier"] = outlier
+        if modeForGrafic == "grafic":
+            while outlier <= 0.25:
+                LSsaver = []
+                MCDsaver_ = []
+                Hubersaver = []
+                Cauchysaver = []
 
+                params["outlier"] = outlier
+                for i in range(nCycle):
+                    Y, xAll = modelForData.Main_Model(params=params)
+                    h = int(n * (1 - outlier))
+
+                    xVectorMCD_, yVectorMCD_ = mcdMethod_three_var.FindRelativeDistances(X=xAll, Y=Y, n=n, h=h)
+                    xMatrixMCD_ = self.__filingMatrixX(xall=xVectorMCD_, n=h)
+                    tettaMCD_ = LSObject.LSMatrix(xMatrixMCD_, yVectorMCD_)
+                    MCDsaver_.append(tettaMCD_.copy())
+
+                    X = self.__filingMatrixX(xAll, n)
+                    tettaLS = LSObject.LSMatrix(X, Y)
+                    LSsaver.append(tettaLS.copy())
+
+                    tettaMEstHuber = MObject.MainEstimators(tettaLS, "Huber", X, Y, n)
+                    Hubersaver.append(tettaMEstHuber.copy())
+
+                    tettaMEstCauchy = MObject.MainEstimators(tettaLS, "Cauchy", X, Y, n)
+                    Cauchysaver.append(tettaMEstCauchy.copy())
+                    print(f" i == {i}\n")
+
+                extraObj = ex.ExtraThings()
+                iLS.append(extraObj.MainCount(LSsaver, tetta, nCycle)[0])
+                iMCD.append(extraObj.MainCount(MCDsaver_, tetta, nCycle)[0])
+                iHuber.append(extraObj.MainCount(Hubersaver, tetta, nCycle)[0])
+                iCauchy.append(extraObj.MainCount(Cauchysaver, tetta, nCycle)[0])
+
+                print(f" Outlier == {outlier}\n")
+                outSaver.append(outlier)
+                outlier += 0.03
+
+                # nSaver.append(n)
+                # n += 40
+            self.__CreateGrafic(fileName=f"SFFM_FourMethods_{mode}_n_{n}_nCycle_{nCycle}_points_{len(outSaver)}",
+                                path=f"grafics/{mode}/", outSaverX=outSaver,
+                                paramMethodsY={"iLS": iLS, "iMCD": iMCD, "iHuber": iHuber, "iCauchy": iCauchy},
+                                about=["LS", "MCD", "Huber", "Cauchy"], xlabel="Выбросы", ylabel="Показатель точности")
+            print(outSaver)
+        elif modeForGrafic == "oneOutput":
             for i in range(nCycle):
                 Y, xAll = modelForData.Main_Model(params=params)
                 h = int(n * (1 - outlier))
@@ -108,13 +154,11 @@ class StandForFourMethods:
             iHuber.append(extraObj.MainCount(Hubersaver, tetta, nCycle)[0])
             iCauchy.append(extraObj.MainCount(Cauchysaver, tetta, nCycle)[0])
 
-            print(f" Outlier == {outlier}\n")
-            outSaver.append(outlier)
-            outlier += 0.03
+            zones = params["emissionZones"]
+            message = f"{mode}_n = {n}_nCycle = {nCycle}_outlier = {outlier}_params = {zones[0]}_{zones[1]}_{zones[2]}_{zones[3]}"
+            self.__CreateFileForIndexes(name=f"{mode}_n_{n}_nCycle_{nCycle}_outlier_{outlier}_params_{zones[0]}_{zones[1]}_{zones[2]}_{zones[3]}",
+                                        path=f"dataForIndex/{mode}/",
+                                        LS=iLS, MCD=iMCD, Huber=iHuber, Cauchy=iCauchy, message=message)
 
-            # nSaver.append(n)
-            # n += 40
-        self.__CreateGrafic(fileName="SFFM_FourMethods", path="grafics/", outSaverX=outSaver,
-                            paramMethodsY={"iLS": iLS, "iMCD": iMCD, "iHuber": iHuber, "iCauchy": iCauchy},
-                            about=["LS", "MCD", "Huber", "Cauchy"], xlabel="Выбросы", ylabel="Показатель точности")
-        print(outSaver)
+
+
